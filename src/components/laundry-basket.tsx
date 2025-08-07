55e8fe0416d1e2559424aa974bb8233f0ed06d75
Chun-gu <chungu.yi92@gmail.com>
2025-08-07 13:51:53 +0000

    feat: add `laundry-basket` component

---
+++ src/components/laundry-basket.tsx
@@ -0,0 +1,39 @@
+import { cn } from "@/lib/utils";
+
+import type { Laundry } from "@/types";
+
+interface LaundryBasketProps {
+	laundryList: Array<Laundry>;
+	onClick: (id: Laundry["id"]) => void;
+	selectedLaundrySet: Set<Laundry["id"]>;
+	className?: string;
+}
+
+export const LaundryBasket = ({
+	className,
+	laundryList,
+	onClick,
+	selectedLaundrySet,
+}: LaundryBasketProps) => {
+	return (
+		<ul className={cn("grid grid-cols-2 gap-[16px] pb-[16px]", className)}>
+			{laundryList.map((laundry) => {
+				return (
+					<li key={laundry.id}>
+						{/* 세탁물 카드 */}
+						<div
+							onClick={() => onClick(laundry.id)}
+							className={cn(
+								"aspect-square rounded-[24px] border border-gray-bluegray-2 bg-gray-1",
+								selectedLaundrySet.has(laundry.id) &&
+									"border-[3px] border-main-blue-1",
+							)}
+						>
+							<img src={``} alt="" />
+						</div>
+					</li>
+				);
+			})}
+		</ul>
+	);
+};
