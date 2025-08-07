6e84aeebe3a3c02b7380c056b28ee47b2b44c519
Chun-gu <chungu.yi92@gmail.com>
2025-08-07 13:45:27 +0000

    feat: add `empty-laundry-basket` component

---
+++ src/components/empty-laundry-basket.tsx
@@ -0,0 +1,15 @@
+import EmptyLaundryBasketImg from "@/assets/images/empty-laundry-basket.png";
+import { cn } from "@/lib/utils";
+
+type EmptyLaundryBasketProps = {
+	className?: string;
+};
+
+export const EmptyLaundryBasket = ({ className }: EmptyLaundryBasketProps) => {
+	return (
+		<div className={cn("flex flex-col items-center gap-[25px]", className)}>
+			<img src={EmptyLaundryBasketImg} role="presentation" />
+			<p className="text-body-2 text-gray-1">빨랫감이 없어요</p>
+		</div>
+	);
+};
