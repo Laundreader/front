912f844c32382606031e97b53f791b0a0bfacc32
Chun-gu <chungu.yi92@gmail.com>
2025-08-17 08:35:40 +0000

    feat: add global delay for mock

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,4 +1,11 @@
+import { delay, http } from "msw";
 import { imageHandlers } from "./image";
 import { laundryHandlers } from "./laundry";
 
-export const handlers = [...imageHandlers, ...laundryHandlers];
+export const handlers = [
+	http.all("*", async () => {
+		await delay(1000);
+	}),
+	...imageHandlers,
+	...laundryHandlers,
+];
