85b2def966606565d35d7558b7378da4b4ba82bc
Chun-gu <chungu.yi92@gmail.com>
2025-08-18 20:42:26 +0000

    feat: implement care label analysis and add validation schemas for laundry solutions

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,11 +1,20 @@
-import { delay, http } from "msw";
+import { delay, http, passthrough } from "msw";
 import { imageHandlers } from "./image";
 import { laundryHandlers } from "./laundry";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const handlers = [
-	http.all("*", async () => {
+	http.all(`${API_URL}/laundry/solution/*`, async () => {
+		await delay(10000);
+		// passthrough();
+	}),
+	http.all(`${API_URL}/*`, async () => {
 		await delay(1000);
 	}),
 	...imageHandlers,
 	...laundryHandlers,
+	http.all("*", () => {
+		passthrough();
+	}),
 ];
