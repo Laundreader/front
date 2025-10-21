5977074bf319196344daa2d3b290ecac35e8afd1
Chun-gu <chungu.yi92@gmail.com>
2025-10-21 13:07:42 +0000

    feat: 정책 페이지 완성

---
+++ src/routes/_policies/terms-of-service.tsx
@@ -0,0 +1,32 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { POLICY } from "./-constants";
+import { Policy } from "./-ui";
+
+export const Route = createFileRoute("/_policies/terms-of-service")({
+	component: RouteComponent,
+});
+
+function RouteComponent() {
+	return (
+		<>
+			<Policy.Title title={POLICY.termsOfService.title} />
+
+			<ol className="flex flex-col gap-4">
+				{POLICY.termsOfService.articles.map((article, index) => (
+					<li key={index}>
+						<Policy.Article>
+							<Policy.Subtitle>{article.title}</Policy.Subtitle>
+							<Policy.Content>
+								<ul>
+									{article.content.map((line, lineIndex) => (
+										<li key={lineIndex}>{line}</li>
+									))}
+								</ul>
+							</Policy.Content>
+						</Policy.Article>
+					</li>
+				))}
+			</ol>
+		</>
+	);
+}
